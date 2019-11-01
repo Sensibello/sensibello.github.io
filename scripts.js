80cfc2ddad7765e3174b5f549fef12d00374170c
karli sensibello <45071344+Sensibello@users.noreply.github.com>
2019-11-01 11:39:30 -0400

    2019 scripts update

--- scripts.js
+++ scripts.js
@@ -1,14 +1,67 @@
-(function() {
-	const $clone = document.querySelector('main').cloneNode(true)
-	$clone.id = 'clone'
-	document.body.appendChild($clone)
+//Navbar Toggle
 
-	document.addEventListener('disabled.mousemove', e => {
-		const
-			f = 0.065,
-			x = (-e.pageX / 2) * f,
-			y = (-e.pageY / 2) * f
+$('.navbar-toggle').click(function(){
+  if( $(this).hasClass('collapsed') ){
+    $(this).removeClass('collapsed');
+  }else{
+    $(this).addClass('collapsed');
+  }
+ })
+ 
+ 
+// Back to top Arrow
+ 
+ jQuery(document).ready(function($){
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.cd-top, .top');
 
-		$clone.style.transform = `translate(${x}px, ${y}px) scaleX(-1)`
-	})
-})();
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
+});
+
+
+// handle links with @href started with '#' only
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $(id).offset().top;
+
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos});
+});
+
+
+ 
+
